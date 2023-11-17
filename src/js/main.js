0850dd467c1f48539ca424f9814c6361e6214622
Joel Pereyra <joelpereyra1993@gmail.com>
2023-11-17 21:21:09 +0100

    Started working on HTML file and planning the task and preparing the first design

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
 import "./../scss/style.scss";
 
+
+
+const app = document.getElementById("app"); // Skapar en hook till html div:en
+
+
+const ulLista = document.getElementById("ulLista");
+
+document.getElementById("ulLista").addEventListener("click", function() {
+    forEach(function() {
+        item.classList.add('active')
+    });
+})

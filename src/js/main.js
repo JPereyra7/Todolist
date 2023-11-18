5c885778499f70c2da4b8cd77cfd90b1efdac6a1
Joel Pereyra <joelpereyra1993@gmail.com>
2023-11-18 22:31:32 +0100

    Updated some comments and worked on sass

--- src/js/main.js
+++ src/js/main.js
@@ -5,43 +5,66 @@ const todoList = ["Watch Soccer", "Take out the trash", "Book a dentist appointm
 
 const completedTasks = [];
 
+            // Create a hook to the tasks and completed task ID's
 const todoListContainer = document.getElementById("themTasks");
+const completedContainer = document.getElementById("themFinished");
 
-todoList.forEach((todo) => {
-    const todoContainer = document.createElement("div");
-    const title = document.createElement("h3");
+//First Function
+function createCompletedHtml() {
+    completedContainer.innerHTML = "";
 
-    title.innerHTML = todo;
-    todoContainer.className = "todo";
-    todoContainer.addEventListener("click", () => {
-        completedTasks.push(todo);
-        console.log(completedTasks);
-        createCompletedHtml();
-    });
-    todoContainer.appendChild(title);
-    todoListContainer.appendChild(todoContainer);
-});
+    completedTasks.forEach((completedItem, i) => {
+        const todoContainer = document.createElement("div");
+        const title = document.createElement("h3");
 
-    const completedContainer = document.getElementById("themFinished");
+        title.innerHTML = completedItem;
+        todoContainer.className = "todo";
 
-    const createCompletedHtml = () => {
-        completedContainer.innerHTML = "";
+        todoContainer.addEventListener("click", () => {
+            // Move the task back to the to-do list
+            todoList.push(completedItem);
+            console.log(todoList);
 
-        completedTasks.forEach((completedItem, i) => {
-            const todoContainer = document.createElement("div");
-            const title = document.createElement("h3");
+            completedTasks.splice(i, 1);
 
-            title.innerHTML = completedItem;
-            todoContainer.className = "todo";
-
-            todoContainer.addEventListener("click", () =>{
-                completedTasks.splice(i, 1);
-                createCompletedHtml();
-            });
-
-            todoContainer.appendChild(title);
-            completedContainer.appendChild(todoContainer);
-
+            createCompletedHtml();
+            createTodoListHtml();
         });
-        
-    }
+
+        todoContainer.appendChild(title);
+        completedContainer.appendChild(todoContainer);
+    });
+}
+
+//Second Function
+function createTodoListHtml() {
+    todoListContainer.innerHTML = "";
+
+    todoList.forEach((todo) => {
+        const todoContainer = document.createElement("div");
+        const title = document.createElement("h3");
+
+        title.innerHTML = todo;
+        todoContainer.className = "todo";
+        todoContainer.addEventListener("click", () => {
+            completedTasks.push(todo);
+            console.log(completedTasks);
+            createCompletedHtml();
+
+            // Remove the task from todoList
+            const index = todoList.indexOf(todo);
+            if (index !== -1) {
+                todoList.splice(index, 1);
+            }
+
+            createTodoListHtml();
+        });
+
+        todoContainer.appendChild(title);
+        todoListContainer.appendChild(todoContainer);
+    });
+}
+
+
+createTodoListHtml();
+createCompletedHtml();

e7d630bd4313430a430d74bba4e713d2ef13d72a
Joel Pereyra <joelpereyra1993@gmail.com>
2023-11-20 23:31:43 +0100

    Cleaned the code and added a few comments

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     loadTodos();
 });
 
+//Defining my variabel called Original which is a class object
 class Original {
     task;
 
@@ -13,6 +14,7 @@ class Original {
     }
 }
 
+// Four variabels with string value of each tasks.
 const todoList1 = new Original("Watch Soccer");
 const todoList2 = new Original("Take the trash out");
 const todoList3 = new Original("Book a dentist appointment");
@@ -30,37 +32,43 @@ const buttonID = document.getElementById("buttonID");
 // Clear button event for completed tasks
 const clearButtonID = document.getElementById("clearID");
 
+//Event listener listening for a click to reset completed tasks
 clearButtonID.addEventListener("click", function (e) {
     e.preventDefault();
 
+    //Clears the completedTasks array
     completedTasks.length = 0;
     
     createTodoListHtml();
     createCompletedHtml();
 
+    //Save to local storage
     saveTodos();
 });
 
-// Adding the click event when adding new task
+// Adding the click event when adding new task || using the add button
 buttonID.addEventListener("click", function(e) {
     e.preventDefault();
 
     const newTask = inputID.value;
     if (newTask.trim() !== "") {
         todoList.unshift(newTask);
+
+        //Save to localstorage
         saveTodos();
 
         createTodoListHtml();
     }
+    //Resets/clears the input text written in the textfield
     inputID.value = "";
 });
 
-//First function
+//SaveTodos was created to save the todoList to localstorage
 function saveTodos() {
     localStorage.setItem('todoList', JSON.stringify(todoList));
 }
 
-//Second function
+//loadTodos was created to load the todoList from localstorage
 function loadTodos() {
     const storedTodos = localStorage.getItem('todoList');
 
@@ -76,26 +84,7 @@ function loadTodos() {
     }
 }
 
-//Third function - Input input function and button event
-function createInputHtml() {
-    inputID.innerHTML = "";
-
-    inputWritten.forEach((completedInput, i) => {
-        const inputContainer = document.createElement("div");
-        const title = document.createElement("h3");
-
-        title.innerHTML = completedInput;
-        inputContainer.className = "todo";
-
-        inputContainer.addEventListener("submit", () => {
-            todoList.unshift(completedInput);
-    });
-                inputContainer.appendChild(title);
-            inputID.appendChild(inputContainer);
-        });
-    }
-
-//Fourth Function
+//Function that creates html code for the completed tasks and moves it back to the active list through a click event
 function createCompletedHtml() {
     completedContainer.innerHTML = "";
 
@@ -121,7 +110,7 @@ function createCompletedHtml() {
     });
 }
 
-//Fifth Function
+//Works similar to the function above but this event listens for a click that will send to the completed array
 function createTodoListHtml() {
     todoListContainer.innerHTML = "";
 
@@ -133,6 +122,8 @@ function createTodoListHtml() {
         todoContainer.className = "todo";
         todoContainer.addEventListener("click", () => {
             completedTasks.push(todo);
+
+            //Local storage save
             saveTodos();
             createCompletedHtml();
 
